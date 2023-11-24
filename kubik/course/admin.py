ad02a4514355353baa2d4195e049f16a9c8513ec
Наталья Арлазарова <sic15@yandex.ru>
2023-11-24 16:12:27 +0300

    изменение отображение студентов на курсе

--- kubik/course/admin.py
+++ kubik/course/admin.py
@@ -4,15 +4,11 @@ from django.conf.urls import url
 from django.http.request import HttpRequest
 from django.urls import path
 
-from course.models import Course, TimeTable, Application, Enrollment
+from course.models import Course, TimeTable, Application, Enrollment, StudentCourse
 from user.models import User
 from typing import Any, Union
 from django.shortcuts import get_object_or_404
 
-
-#admin.site.register(Course)
-#admin.site.register(TimeTable)
-#admin.site.register(Enrollment)
 
 def enrollment(obj):
     context = {'student':obj.student, 'course':obj.course}
@@ -20,11 +16,8 @@ def enrollment(obj):
     current_course_max = obj.course.capacity
     if current_course_count < current_course_max:
         Enrollment.objects.create(**context)
-      #  new_course_count = obj.course.enrollers
-      #  if new_course_count > current_course_count:
         obj.approved = 1
         obj.save()
-          #  current_course.enrollers += 1
         obj.course.save()
     #   ниже вариант с удалением экземпляра заявки на курс
     #   Application.objects.filter(id=obj.id).delete()
@@ -61,85 +54,24 @@ class ApplicationAdmin(admin.ModelAdmin):
             return HttpResponseRedirect("../")
         return super().response_change(request, obj)
     
-    
-@admin.register(TimeTable)
+
+class MembershipInline(admin.TabularInline):
+    model = Course.timetable.through
+    list_display=['weekday', 'time']
+
 class TimeTableAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        MembershipInline,
+    ]
+    
+class EnrollmentInline(admin.StackedInline):
+    model = Enrollment
+    extra = 0
 
 @admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
-
-    pass
-
-@admin.register(Enrollment)
-class EnrollmentAdmin(admin.ModelAdmin):
-    pass
-"""
-    def save_model(self, request, obj, form, change):
-        print('метод save в админке')
-        obj.course.enrollers += 1
-        obj.course.save()
-        obj.save()
-
-    def delete_queryset(self, request, queryset):
-        print('метод delete в админке')
-        for obj in queryset:
-            print('удалили студента')
-            obj.course.enrollers -= 1
-            obj.course.save()
-        queryset.delete()
-"""
-
-"""
-
-    def get_urls(self):
-	# метод обработки url, с подстановкой необходимой view.
-
-        urls = super(Application, self).get_urls()
-        custom_urls = [
-            path('get/', self.admin_site.admin_view(self.get_repayment), name='repayment_view'), ]
-        return  custom_urls + urls
-
-    def get_repayment(self, request):
-    	# внутри данного метода (который подставится под запрос url), мы выполним какую-либо логику, и вернем в ответ пользовательский шаблон index.html
-        return render(request, 'index.html', locals())
-
-    # переопределяем атрибут для принудительного использования нашего шаблона:
-    """
-    
-"""
-@admin.register(Enrollment)
-class EnrollmentAdmin(admin.ModelAdmin):
-    change_list_template = "path/to/change_form.html"
-
-
-
-class ItemInline(admin.StackedInline):
-    model = TimeTable
- #   extra = 5
-
-@admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
-    inlines = [ItemInline]
-   # exclude = ('Timetable',)
-   # list_display = ["created", "total_price", "paid"]
-
-   """
-
-
-# class MembershipInline(admin.TabularInline):
-#     model = Course.timetable.through
-#     list_display=['weekday', 'time']
-
-# class TimeTableAdmin(admin.ModelAdmin):
-#     inlines = [
-#         MembershipInline,
-#     ]
-    
-
-# @admin.register(Course)
-# class CourceAdmin(admin.ModelAdmin):
-#     inlines = [
-#         MembershipInline,
-#     ]
-#     exclude = ('timetable',)
+class CourceAdmin(admin.ModelAdmin):
+    inlines = [
+        MembershipInline, EnrollmentInline
+    ]
+    exclude = ('timetable',)
+    fields  = ['title', 'description', 'teacher', 'capacity', 'enrollers']
